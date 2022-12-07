44cb5195677832ee23333216d61e38c731c58ec1
ihuk <ivica@bitwerk.io>
2022-12-07 12:46:59 +0100

    Cleaned up day 7 solution.

--- 2022/7.py
+++ 2022/7.py
@@ -1,26 +1,28 @@
 import sys
-from collections import defaultdict
+
 
 def main():
-    ds = defaultdict(lambda: 0)
     cwd = []
-    for o in [l.strip().split() for l in sys.stdin]:
-        if 'dir' == o[0]:
+    ds = {'/': 0}
+    for l in [l.strip().split() for l in sys.stdin]:
+        if 'dir' == l[0]:
+            ds[''.join(cwd) + l[1]] = 0 
             continue
-        if '$' == o[0]:
-            if 'cd' == o[1]:
-                if '/' == o[2]:
+        if '$' == l[0]:
+            if 'cd' == l[1]:
+                if '/' == l[2]:
                     cwd = ['/']
                     continue
-                if '..' == o[2]:
+                if '..' == l[2]:
                     cwd.pop()
                     continue
-                cwd.append(o[2].strip()) 
+                cwd.append(l[2].strip()) 
             continue
         for p in [''.join(cwd[:i + 1]) for i in range(len(cwd))]:
-            ds[p] += int(o[0])
+            ds[p] += int(l[0])
     print sum([v for k, v in ds.items() if v <= 100000])
     print min([v for k, v in ds.items() if v >= 30000000 - (70000000 - ds['/'])])
+
 
 if '__main__' == __name__:
     main()
